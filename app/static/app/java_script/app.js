ce1d7a56e6d0923a86665e8ccb3d1ef45aabfd7f
vladimir <grun_gespenst@tut.by>
2022-10-07 22:18:28 +0300

    local file does not play

--- app/static/app/java_script/app.js
+++ app/static/app/java_script/app.js
@@ -1,55 +1,15 @@
 $(document).ready (function () {
-    $("#submit").click(function () {
-        $("#meaning").remove();
+    $("#reset").click(function () {
+        document.getElementById("meaning").innerHTML = '';
     });
 });
-
-
-
-// $(document).ready (function () {
-//     $("#play").click(function () {
-//         var myAudioTag = document.getElementById('myTune');
-//         myAudioTag.play();
-//     });
-//
-//
-// });
-
-
-
-// var status = "play";
-// $(document).ready (function () {
-//     $("#play").click(function () {
-//
-//
-//         if (status == 'pause') {
-//             status='play';
-//             alert(status);
-//         } else {
-//             status = 'pause';
-//             alert(status);
-//         }
-//         $("#myTune").trigger(status);
-//     });
-// });
-
-
-// $(document).ready (function () {
-//     $("#play").click(function () {
-//     var myAudio = document.getElementById("myTune");
-//         if (myAudio.paused) {
-//         myAudio.play();
-//         } else {
-//             myAudio.pause();
-//     }
-//     });
-// });
-
-
-// $(document).ready(function() {
-//   $('#play').click(function() {
-//     // var x = document.getElementById("myTune").innerHTML;
-//     alert('fuck');
-//
-//   });
-// });
+$(document).ready (function () {
+    $("#play").click(function () {
+    var myAudio = document.getElementById("myTune");
+        if (myAudio.paused) {
+        myAudio.play();
+        } else {
+            myAudio.pause();
+                }
+    });
+});
--- app/views.py
+++ app/views.py
@@ -1,8 +1,6 @@
 from gtts import gTTS
 from langdetect import detect_langs
-import vlc
 
-from io import BytesIO
 from django.shortcuts import render
 from pathlib import Path
 
@@ -14,6 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 def index(request):
     if request.method == 'POST':
         action = request.POST['meaning']
+        print(action)
         answer = detect_langs(action)[0].__str__().split(':')[0]
         languages = {'en': 'Англійская', 'de': 'Нямецкая', 'fr': 'Французская',
                 'ru': 'Руская', 'uk': 'Украінская', 'pl': 'Польская'
@@ -21,15 +20,19 @@ def index(request):
         detected = languages.get(answer, 'Невядомая мова!')
 
         tts = gTTS(action, lang=answer)
-        tts.save('media/file.mp3')
+        tts.save('media/file.wav')
+
+        domain = request.get_host()
+        path = ''.join(("http://", domain, str(BASE_DIR), '/media', "/file.wav"))
+        print(path, '$$$$$$$$$$$$$$$$$$')
+
+        src = "file:///C:/.../file.wav"
 
 
-        # p = vlc.MediaPlayer(path)
-        # p.play()
 
 
 
-        context = {"answer": detected, "result": action}
+        context = {"answer": detected, "result": action, 'path': path}
         return render(request, 'app/index.html', context=context)
     else:
         context = {"answer": ""}
