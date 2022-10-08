a100c5bc6dd9a929bc54f9b47b7dfbab77bc1954
vladimir <grun_gespenst@tut.by>
2022-10-08 23:15:27 +0300

    deploy_1

--- LanguageIdentifier/settings.py
+++ LanguageIdentifier/settings.py
@@ -154,5 +154,5 @@ if "CI" in os.environ:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'app/', 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'app/', 'media')
+# MEDIA_URL = '/media/'
