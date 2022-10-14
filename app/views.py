d89ac13234e49560c8e62c62c2c7f6fb05761cfa
vladimir <grun_gespenst@tut.by>
2022-10-14 20:37:53 +0300

    form works

--- app/views.py
+++ app/views.py
@@ -7,16 +7,19 @@ from django.core.files.base import ContentFile
 from .models import Track
 
 
-languages = {'en': 'Англійская', 'de': 'Нямецкая', 'fr': 'Французская',
-                'ru': 'Руская', 'uk': 'Украінская', 'pl': 'Польская'
+LANGUAGES = {'en': 'Англійская', 'de': 'Нямецкая', 'fr': 'Французская',
+                'ru': 'Руская', 'uk': 'Украінская', 'pl': 'Польская',
                      }
+TRANSLATE = {'Англійская': 'en', 'Нямецкая': 'de', 'Французская': 'fr',
+                'Руская': 'ru', 'Украінская': 'uk', 'Польская': 'pl',
 
+}
 
 def index(request):
     if request.method == 'POST':
         action = request.POST['meaning']
         answer = detect_langs(action)[0].__str__().split(':')[0]
-        detected = languages.get(answer, 'Невядомая мова!')
+        detected = LANGUAGES.get(answer, 'Невядомая мова!')
         mp3_fp = BytesIO()
         tts = gTTS(action, lang=answer)
         tts.write_to_fp(mp3_fp)
@@ -28,11 +31,14 @@ def index(request):
                          save=False
                         )
         object.save()
-
+        language = request.POST['languages']
+        if language:
+            translate_to = TRANSLATE[language]
+        else:
+            translate_to = 'en'
         translator = Translator()
-        translate = translator.translate(action, src=answer, dest='be')
+        translate = translator.translate(action, src=answer, dest=translate_to)
         translate_text = translate.text
-
         context = {"answer": detected, "result": action,
                    'object': object, "translate_text": translate_text}
         return render(request, 'app/index.html', context=context)
